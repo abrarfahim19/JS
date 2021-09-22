942be52fde9832d305cefc87aca072856c74edf5
abrarfahim19 <fahimpatwari19@gmail.com>
2021-09-22 18:44:55 +0600

    Variable declaration

---
+++ hello.js
@@ -0,0 +1,19 @@
+// The print() of python in Javascript is console.log()
+// For instance
+
+var price = 12;
+console.log(price);
+
+// The variable name is case sensative.
+// The practice is usually CamelCasing.
+// For instance
+
+var bananaPrice = 20;
+var productName = "Chicken";
+var isCheap = true;
+// Find type of variable by simply typing
+
+console.log( typeof bananaPrice )
+console.log( typeof productName )
+console.log( typeof isCheap )
+
