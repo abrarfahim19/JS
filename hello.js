c898f1c8501ba868408b309a56645b1427eed0a3
abrarfahim19 <fahimpatwari19@gmail.com>
2021-09-22 19:32:15 +0600

    Basic operations included

--- hello.js
+++ hello.js
@@ -11,9 +11,73 @@ console.log(price);
 var bananaPrice = 20;
 var productName = "Chicken";
 var isCheap = true;
+
 // Find type of variable by simply typing
 
 console.log( typeof bananaPrice )
 console.log( typeof productName )
 console.log( typeof isCheap )
+
+// Some string operation
+
+var bananaPrice = 20;
+var priceTag = "20"
+var sound = 'kooKKuruuu'
+
+console.log(productName + " " + sound)
+console.log(bananaPrice + " " + sound)
+console.log(bananaPrice + sound)
+
+console.log(bananaPrice + +priceTag)                        // + sign here transformes the string to int
+console.log(bananaPrice + parseInt(priceTag))               // Inteager way to do this
+console.log(bananaPrice + parseFloat(priceTag))             // Floating way to do this
+console.log(bananaPrice + Number(priceTag))                 // Number/NAN way to do this
+
+console.log( sound.toUpperCase() )
+console.log( sound.toLowerCase() )
+console.log( sound.indexOf("u") )
+console.log( sound.indexOf("P") )
+console.log( sound.split("k") )
+
+// Some Number operation
+
+var fractionNumber = 1.2;
+var solidNumber = 2;
+
+console.log( fractionNumber + solidNumber);
+console.log( fractionNumber - solidNumber);
+console.log( fractionNumber * solidNumber);
+console.log( fractionNumber / solidNumber);
+console.log( fractionNumber % solidNumber);
+
+console.log(Math.ceil(fractionNumber))
+console.log(Math.floor(fractionNumber))
+console.log(Math.round(fractionNumber))
+
+console.log(Math.random())
+
+// Conditional is like python.
+
+// if (condition){
+//     do 1
+// }
+// else if(condition2){
+//     do 2
+// }
+// else{
+//     do 3
+// }
+
+var hasWork = true;
+var savings = 100000;
+
+if (hasWork == true && savings >= 100000){
+    console.log("You may get married. Barakallau Fik")
+}
+else if (hasWork ==true || savings >= 100000){
+    console.log("Have some patience Brother! ")
+}
+else{
+    console.log("Pray & Work brother... ")
+}
 
