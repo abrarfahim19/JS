f75a0c90f4dc0d6df103a75d582d11e44fe9745a
abrarfahim19 <fahimpatwari19@gmail.com>
2021-09-25 01:33:36 +0600

    Switch and object added

--- hello.js
+++ hello.js
@@ -56,6 +56,24 @@ console.log(Math.round(fractionNumber))
 
 console.log(Math.random())
 
+// Array Operation
+
+var a = [1,54,6,6,4];
+console.log(a);
+
+a.push(4);
+console.log(a);
+
+a.pop();
+console.log(a);
+
+a.unshift(4)
+console.log(a)
+
+a.shift();
+console.log(a)
+
+
 // Conditional is like python.
 
 // if (condition){
@@ -80,6 +98,34 @@ else if (hasWork ==true || savings >= 100000){
 else{
     console.log("Pray & Work brother... ")
 }
+
+// Switch Case is following
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+console.log("Today is "+ day)
+
 
 // The while/for loop is similar to Python
 
@@ -116,3 +162,23 @@ function factorial(n){
 }
 
 console.log(factorial(5))
+
+// Object is created with properties. The object property can be called through 3 process.
+
+var employee1 = {name:"A", car: "Fiat", sallary: 3000}
+var employee2 = {name:"B", car: "Tesla", sallary: 7000}
+
+
+// Acessing the data
+
+console.log(employee2.car)
+console.log(employee2["car"])
+var key = "car"
+console.log(employee2[key])
+
+// Updating the data
+
+employee2.car = 'porsche';
+employee2["car"] = 'Ford';
+employee2[key]= 'Marcedez'
+console.log(employee2.car)
